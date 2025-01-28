039f46c0391f6ddf4e0baa178a2cff9219735a40
Greg Lemons <greg@flavorstudios.com>
2025-01-28 06:16:53 -0600

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,17 +1,17 @@
 module.exports = {
-  version: "1.0",
   title: "Classy Scrubber",
   description: "Scrub private data with spaCy NLP library and pre-trained models",
-  icon: "icon.png",  
+  icon: "icon.png",
   menu: [{
-    text: "Install",
-    href: "install.json",
-    params: {
-      url: "https://youtube.com/bitterfpv"
-  },
-  {
     text: "Start",
     href: "start.json",
+    params: {
+      url: "https://youtube.com/bitterfpv"
+    }
+  },
+  {
+    text: "Install",
+    href: "install.json",
     params: {
       url: "https://youtube.com/bitterfpv"
     }
